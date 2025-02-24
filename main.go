8a2de54367c0956965ee03243bf44247aba70556
Examples <61053710+Suhach@users.noreply.github.com>
2025-02-24 16:33:28 +0300

    test

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	http.ListenAndServe(":8080", router)
+// -------------------STRUCT----------------------------\\
+type requestTask struct {
+	Task string `json"message"`
 }
 
-// ---------------------------Handler-------------------------//
+// -------------------GLOBAL-VARABLE---------------------\\
+var task string
+
+// -------------------HANDLER-GET-------------------------\\
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello %s", task)
+}
+
+// -------------------HANDLER-POST-------------------------\\
+func SetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var req requestTask
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	task = req.Task
+	fmt.Fprintf(w, "Task updated to %s", task)
+}
+
+// -------------------MAIN-FUNC----------------------------\\
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", SetTaskHandler).Methods("POST")
+
+	http.ListenAndServe(":8080", router)
 }
