3d9696967833767eba9424310ec1671e3c33c3f9
Examples <61053710+Suhach@users.noreply.github.com>
2025-02-24 16:58:34 +0300

    updated

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // -------------------STRUCT----------------------------\\
@@ -16,38 +18,27 @@ var task string
 
 // -------------------HANDLER-GET-------------------------\\
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		fmt.Fprintf(w, "Hello %s", task)
-	} else {
-		fmt.Fprintln(w, "Поддерживается только метод 'GET'")
-	}
+	fmt.Fprintf(w, "Hello %s", task)
 }
 
 // -------------------HANDLER-POST-------------------------\\
 func SetTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var req requestTask
-	if r.Method == http.MethodPost {
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			http.Error(w, "Invalid JSON", http.StatusBadRequest)
-			return
-		}
-		task = req.Task
-		fmt.Fprintf(w, "Task updated to %s", task)
-	} else {
-		fmt.Fprintln(w, "Поддерживается только метод 'POST'")
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
 	}
+	task = req.Task
+	fmt.Fprintf(w, "Task updated to %s", task)
 }
 
 // -------------------MAIN-FUNC----------------------------\\
 func main() {
-	// router := mux.NewRouter()
+	router := mux.NewRouter()
 
-	// router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	// router.HandleFunc("/api/task", SetTaskHandler).Methods("POST")
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", SetTaskHandler).Methods("POST")
 
-	//http.ListenAndServe(":8080", router)
-	http.HandleFunc("/api/hello", HelloHandler)
-	http.HandleFunc("/api/task", SetTaskHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
